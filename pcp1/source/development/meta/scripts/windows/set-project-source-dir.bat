f1798d18b37c1050b8bbcc7a9f160267f7b0ea4b
jrcampins@gmail.com <jrcampins@gmail.com@64cd5813-1f3c-c05e-9850-c8f57e14df9f>
2014-02-04 13:08:17 +0000

    pcp1

---
+++ pcp1/source/development/meta/scripts/windows/set-project-source-dir.bat
@@ -0,0 +1,5 @@
+pushd "%~dp0..\..\..\.."
+set project_source_dir=%CD%
+set project_source_dir
+echo.
+popd
---
+++ pcp1/source/meta-pcp1/src/meta/pcp1/Maestro.java
@@ -0,0 +1,81 @@
+/*
+ * Este programa es software libre; usted puede redistribuirlo y/o modificarlo bajo los terminos
+ * de la licencia "GNU General Public License" publicada por la Fundacion "Free Software Foundation".
+ * Este programa se distribuye con la esperanza de que pueda ser util, pero SIN NINGUNA GARANTIA;
+ * vea la licencia "GNU General Public License" para obtener mas informacion.
+ */
+package meta.pcp1;
+
+import adalid.commons.enums.LoggingLevel;
+import adalid.core.annotations.ProjectModule;
+import adalid.core.enums.Kleenean;
+import java.util.Locale;
+import meta.proyecto.base.ProyectoBase;
+import meta.proyecto.comun.Auditoria;
+import meta.proyecto.comun.ControlAcceso;
+import meta.proyecto.comun.ControlPruebas;
+
+/**
+ * @author Jorge Campins
+ */
+public class Maestro extends ProyectoBase {
+
+    public static void main(String[] args) {
+//      Maestro.setLocale(Locale.ENGLISH);
+        Maestro.setLocale(Locale.forLanguageTag("es"));
+        Maestro.setAlertLoggingLevel(LoggingLevel.OFF);
+//      Maestro.setDetailLoggingLevel(LoggingLevel.INFO);
+//      Maestro.setTrackingLoggingLevel(LoggingLevel.INFO);
+        Maestro maestro = new Maestro();
+        maestro.setDefaultLabel("Gestión de Recursos Empresariales");
+        maestro.setDefaultDescription("Gestión de Recursos Empresariales");
+        maestro.setUserEntityClass(meta.entidad.comun.control.acceso.Usuario.class);
+        if (maestro.build()) {
+            maestro.setAlias("pcp1ap101");
+            maestro.generate("jee1af101"); // jee1af101: netbeans, postgresql, glassfish
+//          maestro.setAlias("pcp1ap102");
+//          maestro.generate("jee1af102"); // jee1af102: netbeans, postgresql, jboss
+//          maestro.setAlias("pcp1ap201");
+//          maestro.generate("jee1af201"); // jee1af201: eclipse,  postgresql, glassfish
+//          maestro.setAlias("pcp1ap202");
+//          maestro.generate("jee1af202"); // jee1af202: eclipse,  postgresql, jboss
+        }
+    }
+
+    @ProjectModule(menu = Kleenean.TRUE, role = Kleenean.TRUE)
+    Modulo1 mod1;
+
+    @ProjectModule(menu = Kleenean.TRUE, role = Kleenean.TRUE)
+    Modulo2 mod2;
+
+    @ProjectModule(menu = Kleenean.TRUE, role = Kleenean.TRUE)
+    Modulo3 mod3;
+
+    @ProjectModule(menu = Kleenean.TRUE, role = Kleenean.TRUE)
+    Modulo4 mod4;
+
+    @ProjectModule(menu = Kleenean.TRUE, role = Kleenean.TRUE)
+    Modulo5 mod5;
+
+    @ProjectModule(menu = Kleenean.TRUE, role = Kleenean.TRUE)
+    Modulo6 mod6;
+
+    @ProjectModule(menu = Kleenean.TRUE, role = Kleenean.TRUE)
+    Modulo7 mod7;
+
+    @ProjectModule(menu = Kleenean.TRUE, role = Kleenean.TRUE)
+    Modulo8 mod8;
+
+    @ProjectModule(menu = Kleenean.TRUE, role = Kleenean.TRUE)
+    Modulo9 mod9;
+
+    @ProjectModule(menu = Kleenean.TRUE, role = Kleenean.TRUE)
+    ControlAcceso modx;
+
+    @ProjectModule(menu = Kleenean.TRUE, role = Kleenean.TRUE)
+    Auditoria mody;
+
+    @ProjectModule(menu = Kleenean.TRUE, role = Kleenean.TRUE)
+    ControlPruebas modz;
+
+}
