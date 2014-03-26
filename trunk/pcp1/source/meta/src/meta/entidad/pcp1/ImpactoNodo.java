5c1a172dd5a9d7b028a3cde0c1ebb4329d553a2a
jrcampins@gmail.com <jrcampins@gmail.com@64cd5813-1f3c-c05e-9850-c8f57e14df9f>
2014-03-26 20:03:21 +0000

    cálculo del peso

---
+++ trunk/pcp1/source/meta/src/meta/entidad/pcp1/ImpactoNodo.java
@@ -0,0 +1,54 @@
+/*
+ * Este programa es software libre; usted puede redistribuirlo y/o modificarlo bajo los terminos
+ * de la licencia "GNU General Public License" publicada por la Fundacion "Free Software Foundation".
+ * Este programa se distribuye con la esperanza de que pueda ser util, pero SIN NINGUNA GARANTIA;
+ * vea la licencia "GNU General Public License" para obtener mas informacion.
+ */
+package meta.entidad.pcp1;
+
+import adalid.core.*;
+import adalid.core.interfaces.*;
+import java.lang.reflect.Field;
+
+/**
+ * ImpactoNodo Persistent Entity.
+ *
+ * @author Jorge Campins
+ */
+public class ImpactoNodo extends meta.entidad.base.PersistentEnumerationEntityBase {
+
+    // <editor-fold defaultstate="collapsed" desc="class constructors">
+    @Deprecated()
+    private ImpactoNodo() {
+        this(null, null);
+    }
+
+    public ImpactoNodo(Artifact declaringArtifact, Field declaringField) {
+        super(declaringArtifact, declaringField);
+    }
+    // </editor-fold>
+
+    @Override
+    protected void settleAttributes() {
+        super.settleAttributes();
+        setDefaultLabel("impacto en el logro del objetivo");
+        setDefaultShortLabel("impacto");
+        setDefaultCollectionLabel("Impactos en el logro del objetivo");
+        setDefaultCollectionShortLabel("Impactos");
+    }
+
+    public Instance MODERADO;
+
+    public Instance ALTO;
+
+    public Instance MUY_ALTO;
+
+    @Override
+    protected void settleInstances() {
+        super.settleInstances();
+        MODERADO.newInstanceField(numero, 1);
+        ALTO.newInstanceField(numero, 3);
+        MUY_ALTO.newInstanceField(numero, 5);
+    }
+
+}
