3c24237dfaaf8da00276b40078b340b84fa1aed3
jrcampins@gmail.com <jrcampins@gmail.com@64cd5813-1f3c-c05e-9850-c8f57e14df9f>
2014-05-02 16:11:15 +0000

    adalid 2277

--- trunk/pcp1/source/management/resources/database/postgresql/functions/variable/after_insert.sql
+++
@@ -1,9 +0,0 @@
-create or replace function variable$after_insert_row$100() returns trigger as $$
-begin
-    insert into rango_variable (id, variable, numeral, expresion)
-    select bigintid(), new.id, numeral, numeral||'. RANGO #'||numeral
-    from generate_series(new.rango_minimo, new.rango_maximo) as numeral
-    order by 1;
-    return null;
-end;
-$$ language plpgsql;
