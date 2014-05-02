615f1a50a7fb95444ad97da2b6e420461bb735ad
jrcampins@gmail.com <jrcampins@gmail.com@64cd5813-1f3c-c05e-9850-c8f57e14df9f>
2014-05-02 16:11:15 +0000

    adalid 2277

--- pcp1/source/management/resources/database/postgresql/functions/nodo_indice/colores.sql
+++
@@ -1,9 +0,0 @@
-create or replace function nodo_indice$insert$colores(_nodo$ bigint) returns void as $$
-declare
-begin
-    insert into color_nodo_indice (id, nodo, color, extremo_inferior)
-    select bigintid(), _nodo$, id, extremo_inferior
-    from color_indice
-    order by 1;
-end;
-$$ language plpgsql;
